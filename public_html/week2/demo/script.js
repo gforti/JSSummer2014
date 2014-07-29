cffe92a33b02e570a8591776c96c660ed6bcd462
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-07-29 19:01:47 -0400

    week2 demo

--- public_html/week2/demo/script.js
+++ public_html/week2/demo/script.js
@@ -1,14 +1,18 @@
 
     var email = document.getElementById('email');
-    
+    var err_email = document.getElementById('err_email');
+    var hasErrors = false;
     
     function submit() {
         
         if ( !email.value.length ) {
             console.log('email is empty');
+            //.classList
+            //innerHTML
         } else {
             console.log('email is not empty');
         }
+        
         
     }
     
