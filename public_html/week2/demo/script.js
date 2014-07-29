b4138f860e218278105f6ad51f9cc8eb8055f94f
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-07-29 18:29:32 -0400

    week2 demo

--- public_html/week2/demo/script.js
+++ public_html/week2/demo/script.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+    var email = document.getElementById('email');
+    
+    console.log(email.value);
+    
+    
+    // method
+    function testing( str ){
+        console.log(str);
+   }
 
+testing('hello');
