f6c3ad2f8444b0f9dd8599f6495917fac9a7f487
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-08-05 17:44:39 -0400

    week3 lab

---
+++ public_html/week3/lab/script.js
@@ -0,0 +1,16 @@
+/* 
+ Create a simple Ad that will display on the page as just text in a div. 
+ The Ad will be random using Math.random from the array length.
+
+Requirements:
+1. Create an array that has 5 ad JSON objects.
+2. Use Math.random() with the array.length to get a random index.
+3. Display the ad using innerHTML.
+4. Replace the document.title with the ad followed by three periods …
+5. Please add comments to explain how the code works and what it’s doing.
+   Hint: No need to go crazy, just general things on the flow, one line
+         comments are fine.
+
+ */
+
+
