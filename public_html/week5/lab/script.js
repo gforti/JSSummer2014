a030e2115feacf124597029458c1d8e62900d1e3
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-08-19 21:51:49 -0400

    week 5 demo

--- public_html/week5/lab/script.js
+++ public_html/week5/lab/script.js
@@ -1,18 +1,19 @@
 /*
  Let’s revisit form validation from week 2. 
-  You will use your same form but create some new JavaScript. 
+ You will use your same form but create some new JavaScript. 
  
 1. In your HTML make a fname and lname fields.
 2. Using regular expression to validate your form.
     a. Full name must be a SpaceAlphaValidate, only characters and spaces allowed
-    c. Email must start with characters, have a @ symbol, have another set 
+    b. Email must start with characters, have a @ symbol, have another set 
        of characters have a period and end with 3 characters.
-    d. Comments must not have any html.  Search for characters that have <> 
+    c. Comments must not have any html.  Search for characters that have <> 
        wrapped around it.
 
 3.  Once all the data is valid hide the form using [object].style.display='none'
     Display a div that will show the data entered.  You can add a div to the page
     and have it display:none to start.
+    div.style.display = 'block';
 
  */
 
