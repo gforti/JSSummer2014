f2cf4bca9c46fc84415c45efb9a6f56a8f4ce2c8
GFORTI <GFORTI@N205INSTR.labs.neit.edu>
2014-09-02 19:26:44 -0400

    week7

--- public_html/week7/Lab/script.js
+++ public_html/week7/Lab/script.js
@@ -6,3 +6,24 @@ $('#nav li a').mouseover(function() {
     $(this).css('background-color', 'white').css('color', 'black')
         .animate({"margin-top": "0px"}, 200 );
 });
+
+
+$("#nav2,#nav3,#nav4").addClass('contentBox');
+
+$("#nav1,#nav2,#nav3,#nav4").hide();
+
+$('.button-color-1 a').click(function(){
+        $('#nav1').show('slow');
+});
+
+$('.button-color-2 a').click(function(){
+        $('#nav2').show('slow');
+});
+
+$('.button-color-3 a').click(function(){
+        $('#nav3').show('slow');
+});
+
+$('.button-color-4 a').click(function(){
+        $('#nav4').show('slow');
+});
